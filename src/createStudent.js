c9d8a133b66a76556474f9abd12ff7524e6aa5c9
Fernanda Vidal <fevidal.dev@gmail.com>
2022-04-06 19:06:35 -0300

    Adiciona requisito 5.

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (stringName) => {
+  let student = {
+    name: stringName,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return student;
+};
+
+console.log(createStudent('Fernanda'));
 
 module.exports = createStudent;
