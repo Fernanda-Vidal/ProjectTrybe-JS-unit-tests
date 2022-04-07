1208c7bad2e216d1bb289622eee933bb106bfa2f
Fernanda Vidal <fevidal.dev@gmail.com>
2022-04-07 10:52:39 -0300

    Adiciona requisito 7.

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,33 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  let result = {
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  };
 
-const arrayGenerator = (type, object) => {};
+  return result;
+};
+console.log(calculator(1, 2));
+
+const arrayGenerator = (type, object) => {
+  if (type === 'keys') {
+    return Object.keys(object);
+    }
+  if (type === 'values') {
+    return Object.values(object);
+  }
+  if (type === 'entries') {
+    return Object.entries(object);
+  }
+};
+
+console.log(arrayGenerator('entries', calculator(1, 2)));
 
 module.exports = { calculator, arrayGenerator };
+
+// let teste = { sum: 3, mult: 2, div: 0, sub: -1 };
+// console.log(Object.keys(teste));
